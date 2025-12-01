3f3ccce34291de4a159795174cda84a95295bc8a
Tanhien1 <hientptran12@gmail.com>
2025-12-01 13:56:03 +0700

    adjust r code to deal with NA (by making them 0s)

--- analysis.R
+++ analysis.R
@@ -1,10 +1,28 @@
 # install.packages("rpca")
+install.packages("rpca")
+# Load the rpca library
 library(rpca)
 
 # Load your CSV file
 # 'header = FALSE' if your CSV is just numbers. Change to TRUE if you have frame names.
 # using as.matrix() is crucial because rpca requires a matrix, not a dataframe.
-video_matrix <- as.matrix(read.csv("frames_pixels.csv", header = FALSE))
+raw <- read.csv(
+  "frames_pixels.csv",
+  header = FALSE,
+  stringsAsFactors = FALSE
+)
+video_matrix <- apply(raw, 2, function(col) as.numeric(col))
+
+
+#check non numeric
+sum(is.na(video_matrix))
+sum(is.infinite(video_matrix))
+
+#replace them with 0
+video_matrix[is.na(video_matrix)] <- 0
+video_matrix[is.infinite(video_matrix)] <- 0
+
+
 
 # Run Robust PCA.
 result <- rpca(video_matrix)
